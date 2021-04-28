f9a82b87528ca6d60ab7e08be015607d30928ea4
joey zeng <289573725@qq.com>
2021-04-29 01:05:57 +0800

    update .gitignore

---
+++ Frameworks/DesignKit1/DesignKit/Classes/Spacing/Spacing.swift
@@ -0,0 +1,16 @@
+//
+//  Spacing.swift
+//  DesignKit
+//
+//  Created by 糖堂 on 2021/4/21.
+//
+
+import Foundation
+
+public struct Spacing {
+    public static let twoExtraSmall: CGFloat = 4
+    public static let small: CGFloat = 12
+    public static let medium: CGFloat = 18
+    public static let large: CGFloat = 24
+    public static let extraLarge: CGFloat = 32
+}
---
+++ JZProject/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  JZProject
+//
+//  Created by 糖堂 on 2021/3/22.
+//
+
+import UIKit
+import DesignKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+     
+        print(Spacing.extraLarge)
+    }
+
+
+}
+
